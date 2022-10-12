2a11deb37e04e4e9b1bafc7d8aae6e859e5ab41d
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-10-12 23:29:30 +0530

    MEDICAL

---
+++ attendance2.py
@@ -0,0 +1,14 @@
+tot= int(input("ENTER TOTAL CLASSES HELD: "))
+PRE=int(input("TOTAL DAYS ATTENDED: "))
+
+att= (PRE/tot)*100
+
+if att>=75:
+    print("YOU ARE ELIGIBLE TO SIT FOR THE EXAM")
+
+else:
+    med=str(input(" do you have any medical cause(Y/N) "))
+    if med == "Y" or med == "y" :
+        print("you are eligible")
+    else:
+        print("you are not eligible")    
