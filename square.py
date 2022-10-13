8659cd45fee3dc00976c21591e3625a9ba1a2a37
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-10-13 21:38:48 +0530

    SQUARE

---
+++ square.py
@@ -0,0 +1,10 @@
+#Take values of length and breadth of a rectangle from user and check if it is square or not.
+ 
+l=int(input("ENTER THE LENGTH: "))
+b= int(input("ENTER THE BREADTH: "))
+
+if l==b:
+    print("IT IS A SQUARE")
+else:
+    print("IT IS NOT A SQUARE")
+        
