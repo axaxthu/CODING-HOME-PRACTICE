14d20c464de5225c2abd4a5477e37761cc0b9628
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-10-23 22:36:01 +0530

    string 1

---
+++ strrrrr.py
@@ -0,0 +1,9 @@
+#enter a number and check how many times n(input) value has occured and multiply the value with the number
+num=int(input())
+nu=str(num)
+n=str(input())
+
+s=nu.count(n)
+print(s)
+p=int(s)*num
+print(p)
