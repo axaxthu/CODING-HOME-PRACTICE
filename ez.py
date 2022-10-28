0bd51367af84704dfd57e626bee3daee9db08661
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-10-28 15:03:09 +0530

    sum and avg

---
+++ ez.py
@@ -0,0 +1,10 @@
+#take 5 integers as input and print its sum and average
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+s=a+b+c+d+e
+avg=s/5
+print("sum:",s)
+print("average:",avg)
