c13b8732d87c573b5c7efc2f1e05052f77686b47
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-11-04 19:45:56 +0530

    COMPARISON OPERATORS

---
+++ comparison.py
@@ -0,0 +1,12 @@
+#input 2 numbers and perform and print all the comparison operators on it
+
+a=int(input("ENTER FIRST NUMBER: "))
+b=int(input("ENTER SECOND NUMBER: "))
+
+print("a<b:",a<b)
+print("a>b:",a>b)
+print("a==b:",a==b)
+print("a!=b:",a!=b)
+print("a>=b:",a!=b)
+print("a>=b:",a>=b)
+print("a<=b:",a<=b)
