f777740fc0ba91f52bf7ea0bbd704f59f175a446
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-11-04 19:38:17 +0530

    UNICODE 2

---
+++ unic2.py
@@ -0,0 +1,7 @@
+#input an integer and print its corresponding character 
+#also add 3 to the integer and print its corresponding character too
+
+n=int(input("INPUT A NUMBER: "))
+print(chr(n))
+n+=3
+print(chr(n))
