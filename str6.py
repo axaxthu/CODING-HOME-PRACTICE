7070e66b59c1f328d90f65d32d4e96d39186a1ec
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-11-05 23:32:28 +0530

    VOWEL LAST

---
+++ str6.py
@@ -0,0 +1,9 @@
+#input a string and check whether last character of the string is vowel or not
+s=str(input())
+l=s[-1]
+
+if l=="a" or l=="e" or l=="i" or l=="o" or l=="u":
+    print("vowel")
+else:
+    print("not vowel")    
+    
