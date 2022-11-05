f11a3782cc44148a4145fad22cfb8b2f2926b16f
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-11-05 23:33:02 +0530

    MAX MIN

---
+++ max.py
@@ -0,0 +1,9 @@
+#input two integers a and b and if their sum is greater than 25 print "maximum" else print "minimum"
+a=int(input("ENTER NUMBER 1: "))
+b=int(input("ENTER NUMBER 2: "))
+s=a+b
+
+if s>25:
+    print("MAXIMUM")
+else:
+    print("MINIMUM")    
